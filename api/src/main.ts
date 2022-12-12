00d031974ea47a07f173d4d1d6acbbb400f3c992
Brian Renwick <89210279+renwickb@users.noreply.github.com>
2022-12-12 15:50:38 +0000

    enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,8 +6,17 @@ import { ConfigService } from "./config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     const configService = app.get(ConfigService);
-    const port = configService.serviceConfig().port();
+    const serviceConfig = configService.serviceConfig();
+
+    const port = serviceConfig.port();
+    const corsOrigins = serviceConfig.corsAllowedOrigins();
+
+    app.enableCors({
+        origin: corsOrigins,
+        credentials: true,
+    });
 
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(port);
